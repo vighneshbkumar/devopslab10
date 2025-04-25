58e78154b025ce0ed832aad03672e43d8e4e3cbe
Your Name <.com>
2025-04-25 09:04:04 +0530

    first commit

---
+++ src/main/java/program10/program1024/App.java
@@ -0,0 +1,13 @@
+package program10.program1024;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
